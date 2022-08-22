626d55d4e05b295b9d5f8b36a969df50c00651fd
manarArabi <manar.araby.ma@gmail.com>
2022-08-22 19:04:48 +0200

    create with status is enabled

--- src/modules/vacancies/controller.js
+++ src/modules/vacancies/controller.js
@@ -2,6 +2,7 @@ import httpStatus from 'http-status'
 import _ from 'lodash'
 import HttpError from '../../common/httpError.js'
 import { Companies } from '../companies/model.js'
+import { VACANCY_OPEN_STATUS } from './constants.js'
 import { Vacancies } from './model.js'
 
 const { UNPROCESSABLE_ENTITY, CREATED } = httpStatus
@@ -10,7 +11,7 @@ export const VacanciesController = {
   createVacancy: async (req, res, next) => {
     try {
       const {
-        body: { title, description, yearsOfExperience },
+        body: { title, description, yearsOfExperience, status = VACANCY_OPEN_STATUS },
         user: { _id: userId }
       } = req
 
@@ -20,7 +21,7 @@ export const VacanciesController = {
         throw HttpError({ message: 'The caller is not an admin for any registered company', status: UNPROCESSABLE_ENTITY })
       }
 
-      const vacancy = await Vacancies.create({ title, description, yearsOfExperience, companyId: company._id, authorId: userId })
+      const vacancy = await Vacancies.create({ title, description, status, yearsOfExperience, companyId: company._id, authorId: userId })
 
       return res.json(vacancy).status(CREATED).send()
     } catch (err) {
--- src/modules/vacancies/validation.js
+++ src/modules/vacancies/validation.js
@@ -1,11 +1,13 @@
 import Joi from 'joi'
+import { VACANCY_STATUSES } from './constants.js'
 
 export const VacanciesRequestValidations = {
   createVacancy: {
     body: {
       title: Joi.string().trim().min(3).required(),
       description: Joi.string().trim().min(3).required(),
-      yearsOfExperience: Joi.number().required()
+      yearsOfExperience: Joi.number().required(),
+      status: Joi.string().valid(...VACANCY_STATUSES)
     }
   }
 }
